8aaee4621962efee0428bd2718c89efd10b2dfa5
MariuszRudnik <86014393+MariuszRudnik@users.noreply.github.com>
2024-11-24 00:21:46 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-
-import './App.css'
+import "./App.css";
 import Stopwatch from "./Stopwatch/Stopwatch.tsx";
 
 function App() {
-
-
+  console.log("App.tsx: App()");
   return (
     <>
       <Stopwatch />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
