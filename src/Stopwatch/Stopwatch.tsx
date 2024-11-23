97fff51690cef279e34a7ba42fdf21883e218f97
MariuszRudnik <86014393+MariuszRudnik@users.noreply.github.com>
2024-11-24 00:30:11 +0100

    Test pre-commit

--- src/Stopwatch/Stopwatch.tsx
+++ src/Stopwatch/Stopwatch.tsx
@@ -1,9 +1,12 @@
-
+import { useRef, useState } from "react";
 
 function Stopwatch() {
-    return (
-        <div></div>
-    );
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const initervalRef = useRef(null);
+  const startTimeRef = useRef(0);
+
+  return <div></div>;
 }
 
 export default Stopwatch;
