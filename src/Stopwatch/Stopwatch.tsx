ce35a619856704603bde3e717d8a19e52feddc64
MariuszRudnik <86014393+MariuszRudnik@users.noreply.github.com>
2024-11-24 00:30:11 +0100

    Add state management and refs to enhance Stopwatch component logic.

--- src/Stopwatch/Stopwatch.tsx
+++ src/Stopwatch/Stopwatch.tsx
@@ -1,9 +1,41 @@
-
+import { useEffect, useRef, useState } from "react";
+import "./Stopwatch.scss";
 
 function Stopwatch() {
-    return (
-        <div></div>
-    );
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const initervalRef = useRef(null);
+  const startTimeRef = useRef(0);
+
+  useEffect(() => {}, [isRunning]);
+
+  function start() {}
+  function stop() {}
+  function reset() {}
+  function lap() {}
+  function formatTime() {
+    return "00:00:00";
+  }
+
+  return (
+    <div className="stopwatch">
+      <div className="display">{formatTime()}</div>
+      <div className="controls">
+        <button className="start-button" onClick={start}>
+          Start
+        </button>
+        <button className="stop-button" onClick={stop}>
+          Stop
+        </button>
+        <button className="reset-button" onClick={reset}>
+          Reset
+        </button>
+        <button className="lap-button" onClick={lap}>
+          Lap
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Stopwatch;
